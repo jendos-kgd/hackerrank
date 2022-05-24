3ea7e9fbadfc679c2f9b200d574add4bc4f4f480
Evgeny Danileyko <56121374+jendos-kgd@users.noreply.github.com>
2022-05-24 15:31:01 +0200

    Add files via upload

---
+++ hackerrank.js
@@ -0,0 +1,142 @@
+/*
+Given a  2D Array, :
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+An hourglass in  is a subset of values with indices falling in this pattern in 's graphical representation:
+
+a b c
+  d
+e f g
+There are  hourglasses in . An hourglass sum is the sum of an hourglass' values. Calculate the hourglass sum for every hourglass in , then print the maximum hourglass sum. The array will always be .
+
+Example
+
+
+-9 -9 -9  1 1 1 
+ 0 -9  0  4 3 2
+-9 -9 -9  1 2 3
+ 0  0  8  6 6 0
+ 0  0  0 -2 0 0
+ 0  0  1  2 4 0
+The  hourglass sums are:
+
+-63, -34, -9, 12, 
+-10,   0, 28, 23, 
+-27, -11, -2, 10, 
+  9,  17, 25, 18
+The highest hourglass sum is  from the hourglass beginning at row , column :
+
+0 4 3
+  1
+8 6 6
+Note: If you have already solved the Java domain's Java 2D Array challenge, you may wish to skip this challenge.
+
+Function Description
+
+Complete the function hourglassSum in the editor below.
+
+hourglassSum has the following parameter(s):
+
+int arr[6][6]: an array of integers
+Returns
+
+int: the maximum hourglass sum
+Input Format
+
+Each of the  lines of inputs  contains  space-separated integers .
+
+Constraints
+
+Output Format
+
+Print the largest (maximum) hourglass sum found in .
+
+Sample Input
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 2 4 4 0
+0 0 0 2 0 0
+0 0 1 2 4 0
+Sample Output
+
+19
+
+*/
+
+// Solution
+
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'hourglassSum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+function hourglassSum(arr) {
+  // Write your code here
+  const summArr = [];
+  for (let i = 1; i < 5; i++) {
+    for (let j = 1; j < 5; j++) {
+      const summ =
+        arr[i - 1][j - 1] +
+        arr[i - 1][j] +
+        arr[i - 1][j + 1] +
+        arr[i][j] +
+        arr[i + 1][j - 1] +
+        arr[i + 1][j] +
+        arr[i + 1][j + 1];
+      summArr.push(summ);
+    }
+  }
+  return Math.max(...summArr);
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  let arr = Array(6);
+
+  for (let i = 0; i < 6; i++) {
+    arr[i] = readLine()
+      .replace(/\s+$/g, "")
+      .split(" ")
+      .map((arrTemp) => parseInt(arrTemp, 10));
+  }
+
+  const result = hourglassSum(arr);
+
+  ws.write(result + "\n");
+
+  ws.end();
+}
